09ebfdf252c3058446543ee079ce47cda55d5ad2
Rohit Jadhav <jadhavrohit9634@gmail.com>
2021-06-06 18:10:41 +0530

    project commit

--- src/main/java/com/rjtech/ppmtool/security/SecurityConstants.java
+++ src/main/java/com/rjtech/ppmtool/security/SecurityConstants.java
@@ -8,5 +8,5 @@ public class SecurityConstants {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final int TOKEN_PREFIX_LENGTH = 7;
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 300_000;
+    public static final long EXPIRATION_TIME = 600_000;
 }
